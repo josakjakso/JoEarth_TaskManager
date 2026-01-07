01428ec486a8955a18b4e081d40b0273c92cb3df
josak <nongpuk2546@gmail.com>
2026-01-07 22:20:15 +0700

    pxg config

--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/Zexono/JoEarth_TaskManager/routes"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 )
@@ -17,13 +18,30 @@ func main() {
 	// }
 
 	godotenv.Load()
-	conn, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	// conn, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	// if err != nil {
+	// 	log.Fatalf("Failed to connect to the database: %v", err)
+	// }
+	// defer conn.Close()
+
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		log.Fatal("DATABASE_URL is not set in environment variables")
+	}
+
+	config, err := pgxpool.ParseConfig(databaseURL)
+	if err != nil {
+		log.Fatalf("Unable to parse DATABASE_URL: %v", err)
+
+	}
+	config.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeSimpleProtocol
+
+	conn, err := pgxpool.NewWithConfig(context.Background(), config)
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
 	}
 	defer conn.Close()
 
-	// Example query to test connection
 	var version string
 	if err := conn.QueryRow(context.Background(), "SELECT version()").Scan(&version); err != nil {
 		log.Fatalf("Query failed: %v", err)
